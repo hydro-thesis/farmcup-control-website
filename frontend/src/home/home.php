9552ff2b6483a5c2427634c8b1d5a6fe25a98ad5
leandrogrng <leandro.garing@gmail.com>
2024-04-10 09:32:50 +0800

    added-new-file

---
+++ frontend/src/home/home.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <!-- Add your CSS links here -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-components-web/10.0.0/material-components-web.min.css">
+    <link rel='stylesheet' href='./homestyle.css'>
+</head>
+<body>
+    <div class="sidenav">
+        <a>Home</a>
+        <a>Cocopeat Moisture</a>
+        <a>Total Dissolved Solids</a>
+        <a>pH Level of Water</a>
+        
+    </div>
+
+    <div class="main">  
+        <h1> </h1>
+        
+    </div>
+
+    <script src='homescript.js'></script>
+</body>
+</html>
---
+++ frontend/src/home/page1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Dashboard </title>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/material-components-web/10.0.0/material-components-web.min.css">
+
+        <link rel='stylesheet' href = './homestyle.css'>
+
+    </head>
+    <body>
+        page 1
+    </body>
+
+</html>
